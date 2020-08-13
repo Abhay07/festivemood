2b078985797e5a97c5f0c4eabefe710033edd2ea
Abhay Srivastav <abhaysrivastav007@gmail.com>
2020-08-13 22:58:09 +0530

    fix

--- independence-day/script.js
+++ independence-day/script.js
@@ -1,35 +1,43 @@
 const languageJson = {
 	1:{
 		wish:'आपको शुभकामनाएं देते हैं',
-		ind:'74 वें स्वतंत्रता दिवस की'
+		ind:'74 वें स्वतंत्रता दिवस की',
+		msg:'स्वतंत्रता दिवस के अवसर पर मेरी ओर से विशेष शुभकामनाएँ'
 	},
 	2:{
 		wish:"wishes you",
-		ind:"Happy 74th Indepence Day"
+		ind:"Happy 74th Indepence Day",
+		msg:"Here's a special wish Independence Day wish from me"
 	},
 	3:{
 		wish:"আপনাকে শুভেচ্ছা দেয়",
-		ind:"Th৪ তম স্বাধীনতা দিবসের"
+		ind:"Th৪ তম স্বাধীনতা দিবসের",
+		msg:"স্বাধীনতা দিবস উপলক্ষে আমার পক্ষ থেকে বিশেষ শুভেচ্ছা"
 	},
 	4:{
 		wish:"ତୁମକୁ ଇଚ୍ଛା ଦିଏ |",
-		ind:"74 ତମ ସ୍ୱାଧୀନତା ଦିବସର |"
+		ind:"74 ତମ ସ୍ୱାଧୀନତା ଦିବସର |",
+		msg:"ସ୍ୱାଧୀନତା ଦିବସ ଅବସରରେ ମୋ ତରଫରୁ ବିଶେଷ ଶୁଭେଚ୍ଛା |"
 	},
 	5:{
 		wish:"तुम्हाला शुभेच्छा देतो",
-		ind:"74 व्या स्वातंत्र्य दिनाच्या"
+		ind:"74 व्या स्वातंत्र्य दिनाच्या",
+		msg:"स्वातंत्र्य दिनाच्या निमित्ताने माझ्याकडून विशेष शुभेच्छा"
 	},
 	6:{
 		wish:"ನಿಮಗೆ ಶುಭಾಶಯಗಳನ್ನು ನೀಡುತ್ತದೆ",
-		ind:"74 ನೇ ಸ್ವಾತಂತ್ರ್ಯ ದಿನ"
+		ind:"74 ನೇ ಸ್ವಾತಂತ್ರ್ಯ ದಿನ",
+		msg:"ಸ್ವಾತಂತ್ರ್ಯ ದಿನದಂದು ನನ್ನಿಂದ ವಿಶೇಷ ಹಾರೈಕೆ"
 	},
 	7:{
 		wish:"உங்களுக்கு விருப்பங்களைத் தருகிறது",
-		ind:"74 வது சுதந்திர தினத்தில்"
+		ind:"74 வது சுதந்திர தினத்தில்",
+		msg:"சுதந்திர தினத்தையொட்டி என்னிடமிருந்து சிறப்பு ஆசை"
 	},
 	8:{
 		wish:"మీకు శుభాకాంక్షలు ఇస్తుంది",
-		ind:"74 వ స్వాతంత్ర్య దినోత్సవం"
+		ind:"74 వ స్వాతంత్ర్య దినోత్సవం",
+		msg:"స్వాతంత్ర్య దినోత్సవం సందర్భంగా నా నుండి ప్రత్యేక కోరిక"
 	}
 }
 const userName= document.querySelector("#userName");
@@ -38,7 +46,10 @@ const shareIcon = document.querySelector("#shareIcon");
 const indText = document.querySelector("#ind");
 const url = window.location.href;
 const splits = url.split('?name=');
-changeLanguage({target:{value:1}})
+const langSplits = url.split('lang=');
+const languageSelect = document.querySelector('select');
+const userInput = document.querySelector('input');
+let language;
 if(splits && splits[1]){
 	const name = decodeURIComponent(splits[1]);
 	setName({
@@ -49,8 +60,17 @@ if(splits && splits[1]){
 }
 else{
 	hideName();
-	shareIcon.href = `whatsapp://send?text=🇮🇳Here's a special Independent Day Wish from me 🇮🇳 ${url}`
 }
+
+if(langSplits && langSplits[1]){
+	language = langSplits[1];
+}
+else{
+	language = 2;
+}
+changeLanguage({target:{value:language}});
+languageSelect.value = language;
+
 function playAudio(){
 	const audio = document.querySelector('audio');
 	if(audio.paused){
@@ -70,7 +90,6 @@ function setName(e){
 	const name = e.target.value;
 	userName.innerHTML = name;
 	const url = window.location.origin+'/independence-day';
-	shareIcon.href = `whatsapp://send?text=🇮🇳 Here's a special Independent Day Wish from me 🇮🇳 ${url}?name=${encodeName(name)}`
 }
 
 function hideName() {
@@ -92,8 +111,12 @@ function decodeName(t){
 }
 
 function changeLanguage(e){
-	wishText.innerHTML = languageJson[e.target.value].wish;
-	indText.innerHTML = languageJson[e.target.value].ind;
+	language = e.target.value;
+	wishText.innerHTML = languageJson[language].wish;
+	indText.innerHTML = languageJson[language].ind;
 }
 
-
+function share(e){
+	const msg = `whatsapp://send?text=🇮🇳 ${languageJson[language].msg} 🇮🇳 ${window.location.origin}?name=${encodeName(userInput.value)}${encodeURIComponent('&')}lang=${language}`;
+	e.target.href = msg;
+}
